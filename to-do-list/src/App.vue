f6a7f10b286da5179b59f7bec8770550439df9f0
kboysm <lishal12@naver.com>
2020-02-21 00:51:56 +0900

    to-do-list_1

---
+++ to-do-list/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<v-container>
+  {{todoList}}
+  <v-layout row wrap>
+    <v-row >
+      <v-col xs6>
+        <List />
+      </v-col>
+    </v-row>
+    <v-row >
+      <v-col xs6>
+        <ListAdd 
+        @listAdd="listAdd"
+        />
+      </v-col>
+    </v-row>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+import List from './views/List'; 
+import ListAdd from './views/ListAdd'; 
+export default {
+  components:{
+    List,
+    ListAdd
+  },
+  data: () => ({
+    todoList:[],
+  }),
+  methods:{
+    listAdd(memo){
+      console.log('잘 받음',memo);
+      this.todoList.push({memo:memo,status:"created"});
+    }
+  }
+};
+</script>
---
+++ to-do-list/src/views/ListAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-textarea
+    outline
+    v-model="memo"
+    label="to-do-list를 입력해주세요"
+    value=""
+    >
+    </v-textarea>
+    <v-btn @click="listAdd">추가하기</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      memo:null
+    }
+  },
+  methods:{
+    listAdd(){
+      console.log('리스트 추가')
+      if(this.memo ===null){
+        alert('할일을 입력해주세요.')
+      }else{
+      this.$emit('listAdd',this.memo);
+      }
+    }
+  }
+
+}
+</script>
