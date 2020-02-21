294c6f9861afefb151d97a6a4b3e2e7f01e0e6b9
kboysm <lishal12@naver.com>
2020-02-22 01:56:19 +0900

    re_study_vuex_first

---
+++ vuex-project/src/components/Users/AllUsers.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>All Users</h1>
+    <v-list two-line>
+      <v-list-tile 
+        v-for="(user, index) in $store.state.allUsers"
+        :key="index"
+        avatar
+      >
+        <v-list-tile-avatar color="grey lighten-3">
+          <img :src="user.src">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title v-html="user.name"></v-list-tile-title>
+          <v-list-tile-sub-title>id:#{{index}} / {{user.address}} 거주</v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+  </div>
+</template>
+
+<script>
+import { EventBus } from '@/main.js'
+
+  export default {
+    data() {
+      return {
+       
+      }
+    },
+    mounted() {
+      EventBus.$on('signUp', users => {
+        this.$store.state.allUsers.push(users)
+      })
+    }
+  }
+</script>
