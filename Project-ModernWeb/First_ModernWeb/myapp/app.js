e122adec503ad708fee4c36d4cbef90f7e3929c7
kboysm <lishal12@naver.com>
2020-02-25 00:51:29 +0900

    기본 모던웹 틀 완성

--- Project-ModernWeb/First_ModernWeb/myapp/app.js
+++ Project-ModernWeb/First_ModernWeb/myapp/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var setRouter = require('./routes/set');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var setRouter = require('./routes/set');
 
 var app = express();
 
@@ -18,11 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'fe','dist'))); //정적 페이지 vuetify
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/set', setRouter);
+app.use('/api', require('./routes/api')); //api서버
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/set', setRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
