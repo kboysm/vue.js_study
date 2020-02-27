eb204189de649205b20aba885bfde133112c6bef
kboysm <lishal12@naver.com>
2020-02-27 19:59:38 +0900

    Promise

--- Project-ModernWeb/First_ModernWeb/second_back-front-Separation/be/app.js
+++ Project-ModernWeb/First_ModernWeb/second_back-front-Separation/be/app.js
@@ -83,42 +83,82 @@ const key = 'veryNiceKey'
 // console.log(r)
 // });
 
-User.findOne({name:'aaa', age:15},(err,u)=>{
-  if(err) return console.error(err.message)
-  if(!u){
-    console.log(u) //null
-    User.create({name:'aaa',age:15},(err,cu)=>{
-      if(err) return console.error(err.message)
-      console.log(cu);
-      jwt.sign({name:cu.name,age:cu.age},key,(err,token)=>{
-        if(err) return console.error(err.message)
-        console.log(token)
-        jwt.verify(token,key,(err,v)=>{
-          if(err) return console.error(err.message)
-          console.log(v)
-        })
-      })
+// User.findOne({name:'aaa', age:15},(err,u)=>{
+//   if(err) return console.error(err.message)
+//   if(!u){
+//     console.log(u) //null
+//     User.create({name:'aaa',age:15},(err,cu)=>{
+//       if(err) return console.error(err.message)
+//       console.log(cu);
+//       jwt.sign({name:cu.name,age:cu.age},key,(err,token)=>{
+//         if(err) return console.error(err.message)
+//         console.log(token)
+//         jwt.verify(token,key,(err,v)=>{
+//           if(err) return console.error(err.message)
+//           console.log(v)
+//         })
+//       })
+//     })
+//   }
+//   else {
+//     console.log(u)
+//     const user =u
+//     User.update({_id:u._id},{$inc:{age:1}},(err,ur)=>{ //$inc는 해당 {속성:값} -> 속성값에 +1 더해준다는 의미
+//       if(err) return console.error(err.message)
+//       console.log(ur)
+//       jwt.sign({name:user.name,age:user.age+1},key,(err,token)=>{
+//         if(err) return console.error(err.message)
+//         console.log(token)
+//         jwt.verify(token,key,(err,v)=>{
+//           if(err) return console.error(err.message)
+//           console.log(v)
+//         })
+//       })
+//     })
+//   }
+// })
+
+                                    // promise //
+const signToken = (u,k)=>{
+  return new Promise((resolve,reject)=>{
+    jwt.sign({name:u.name , age:u.age},k,(err,token)=>{
+      if(err) reject(err)
+      resolve(token)
     })
-  }
-  else {
-    console.log(u)
-    const user =u
-    User.update({_id:u._id},{$inc:{age:1}},(err,ur)=>{ //$inc는 해당 {속성:값} -> 속성값에 +1 더해준다는 의미
-      if(err) return console.error(err.message)
-      console.log(ur)
-      jwt.sign({name:user.name,age:user.age+1},key,(err,token)=>{
-        if(err) return console.error(err.message)
-        console.log(token)
-        jwt.verify(token,key,(err,v)=>{
-          if(err) return console.error(err.message)
-          console.log(v)
-        })
-      })
+  })
+}
+const verifyToken = (t,k) =>{
+  return new Promise((resolve,reject)=>{
+    jwt.verify(t,k,(err,v)=>{
+      if(err) reject(err)
+      resolve(v)
     })
-  }
-})
+  })
+}
 
 
+let user
+User.findOne({name:'aaa'})
+  .then((u)=>{
+    if(!u) return User.create({name:'aaa',age:12}) //이녀석도 프라미스이기 때문에 아래then으로 넘어감
+    return Promise.resolve(u)
+  })
+  .then((u)=>{
+    user =u
+    return User.update({_id:u._id},{$inc:{age:1}})
+  })
+  .then((r)=>{
+    if(!r.nModified) throw new Error('수정된 것이 없어요')
+    user.age++
+    return signToken(user,key)
+  })
+  .then((token)=>{
+    return verifyToken(token,key)
+  })
+  .then(v=>console.log(v))
+  .catch((err)=>{
+    console.error(err.message)
+  })
 
 
 //예전에는 주로 이렇게 코드를 작성했으니 요즘에는 이렇게 하지 않는다!
