cc9b8f2ff7e5aa97580f93ec956db26428e72e24
kboysm <lishal12@naver.com>
2020-02-27 19:21:26 +0900

    complex-callback

--- Project-ModernWeb/First_ModernWeb/second_back-front-Separation/be/app.js
+++ Project-ModernWeb/First_ModernWeb/second_back-front-Separation/be/app.js
@@ -36,6 +36,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+/////////////////////////////////////// mongoose //////////////////////////////////////////////////////
 const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true);
 const User = require('./models/users')
@@ -54,7 +56,9 @@ mongoose.connect('mongodb://localhost:27017/nemv',{ useNewUrlParser: true, useUn
   // .catch(e =>console.log(e));
   // User.deleteOne({name:'하하'}).then(r => console.log(r)).catch(e =>console.log(e));
 })
+///////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////// jwt /////////////////////////////////////////
 var jwt = require('jsonwebtoken');
 const key = 'veryNiceKey'
 var token = jwt.sign({ name: 'lsm',email:'oop@naver.com' }, key);
@@ -63,3 +67,44 @@ var decoded = jwt.verify(token, key);
 console.log(decoded) // 출력 결과 : { name: 'lsm', email: 'oop@naver.com', iat: 1582795982 } 
 // iat는 발행한 시간을 의미
 console.log(new Date(decoded.iat * 1000))//날짜 시간정보 출력
+
+///////////////////////////////////////////////////////////////////////////////
+
+////////////////////////// 콜백 ////////////////////////////////////
+
+const foo = (v,cb) =>{
+  if(v>1) return cb(new Error('abcd'))
+  setTimeout(()=>{
+    cb(null,v+1)
+  },3000)
+}
+foo(1,(err,r)=>{
+if(err) return console.error(err.message)
+console.log(r)
+});
+
+User.findOne({name:'aaa', age:15},(err,u)=>{
+  if(err) return console.error(err.message)
+  if(!u){
+    console.log(u) //null
+    User.create({name:'aaa',age:15},(err,cu)=>{
+      if(err) return console.error(err.message)
+      console.log(cu);
+      jwt.sign({name:cu.name,age:cu.age},key,(err,token)=>{
+        if(err) return console.error(err.message)
+        console.log(token)
+        jwt.verify(token,key,(err,v)=>{
+          if(err) return console.error(err.message)
+          console.log(v)
+        })
+      })
+    })
+  }
+})
+
+
+
+
+//예전에는 주로 이렇게 코드를 작성했으니 요즘에는 이렇게 하지 않는다!
+//왜 ? promise때문에
+/////////////////////////////////////////////////////////////////////////////////////
