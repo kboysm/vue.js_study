5be8d8bda0f9683e5b78ffa3e4638536962e19fa
kboysm <lishal12@naver.com>
2020-03-01 17:46:12 +0900

    crypto

--- Project-ModernWeb/First_ModernWeb/second_back-front-Separation/fe/src/main.js
+++ Project-ModernWeb/First_ModernWeb/second_back-front-Separation/fe/src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import axios from 'axios'
+
 Vue.config.productionTip = false
 
-Vue.prototype.$axios = axios
-const apiRootPath = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/' : '/api/'
-Vue.prototype.$apiRootPath = apiRootPath
+
 new Vue({
   router,
   store,
