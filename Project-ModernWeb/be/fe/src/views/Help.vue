71c0bb19d56c984011d258ed688a044c50198d48
kboysm <lishal12@naver.com>
2020-03-06 00:20:46 +0900

    vue grammar

--- Project-ModernWeb/be/fe/src/views/Help.vue
+++ Project-ModernWeb/be/fe/src/views/Help.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an help page</h1>
+  <div>
+    <h1>LSM {{title}}</h1>
+    <v-btn :color="btnColor" @click="test">test</v-btn>
+    <v-chip
+    v-for="(item,index) in chips"
+    :key="index"
+    :color='item.cl'
+    >{{item.name}}</v-chip>
   </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            title:'good',
+            btnColor:'success',
+            chips:[{cl:'primary',name:'aaaa'},{cl:'secondary',name:'bbbb'},{cl:'green',name:'cccc'},{cl:'warning',name:'dddd'}]
+        }
+    },
+    methods: {
+        test(){
+            if(this.title === 'good') this.title ='so so'
+            else this.title='good'
+        }
+    },
+}
+</script>
