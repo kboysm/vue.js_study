c8e6bee0504ae2ee31cfbf9002d93fb7e426eed9
kboysm <lishal12@naver.com>
2020-03-08 15:32:57 +0900

    login jwt login middleware

--- Project-ModernWeb/nemv3/be/routes/api/index.js
+++ Project-ModernWeb/nemv3/be/routes/api/index.js
@@ -1,6 +1,28 @@
 var express = require('express');
+const jwt = require('jsonwebtoken');
+const cfg = require('../../../config/conf')
 var router = express.Router();
 var createError = require('http-errors'); //상단에 추가
+router.use('/sign', require('./sign'))
+
+const verifyToken = (t) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(t, cfg.secretKey, (err, v) => {
+      if (err) reject(err)
+      resolve(v)
+    })
+  })
+}
+router.all('*', function(req, res, next) {
+  // 토큰 검사
+  const token = req.headers.authorization
+  verifyToken(token)
+    .then(v => {
+      console.log(v)
+      next()
+    })
+    .catch(e => res.send({ success: false, msg: e.message }))  
+});
 
 router.all('*', function(req, res, next) {  //미들웨어
   if (req.path === '/xxx') return res.send({ msg: '보안상 여긴 못와요!' })
@@ -12,11 +34,6 @@ router.get('/', function(req, res, next) {
 });
 router.get('/hello', function(req, res, next) {
   res.send( { success: true ,msg:'hello'});
-});
-router.use('/sign',require('./sign'))
-router.all('*', function(req, res, next) {
-  console.log('토큰 검사 하는 미들웨어')
-  next();
 });
 
 router.use('/test',require('./test'))
--- Project-ModernWeb/nemv3/fe/src/views/header.vue
+++ Project-ModernWeb/nemv3/fe/src/views/header.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
     <v-btn @click="headerSend">헤더 전송</v-btn>
+    <v-btn @click="apiWithToken">토큰과 함께 전송</v-btn>
+    <v-btn @click="apiWithTrash">이상한 문자와 함께 전송</v-btn>
   </v-container>
 </template>
 <script>
@@ -12,6 +14,17 @@ export default {
       axios.get(`${this.$apiRootPath}test`, { headers: { Authorization: 'fake token!' } })
         .then(r => console.log(r))
         .catch(e => console.log(e))
+    },
+    apiWithToken () {
+      const token = localStorage.getItem('token')
+      axios.get(`${this.$apiRootPath}test`, { headers: { Authorization: token } })
+        .then(r => console.log(r.data))
+        .catch(e => console.log(e.message))
+    },
+    apiWithTrash () {
+      axios.get(`${this.$apiRootPath}test`, { headers: { Authorization: 'abcdefghijk' } })
+        .then(r => console.log(r.data))
+        .catch(e => console.log(e.message))
     }
   },
   mounted () {
