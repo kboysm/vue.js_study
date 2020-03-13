26d54be59558383dbe03eb3e72ce3f6efc02df24
kboysm <lishal12@naver.com>
2020-03-14 01:21:18 +0900

    be/api/article/index re-

--- Project-ModernWeb/nemv3/be/routes/api/article/index.js
+++ Project-ModernWeb/nemv3/be/routes/api/article/index.js
@@ -34,29 +34,40 @@ router.post('/:_board', (req, res, next) => {
 
 router.get('/list/:_board', (req, res, next) => {
   const _board = req.params._board
+  let { search, sort, order, skip, limit } = req.query
+  if (!(sort && order && skip && limit)) return res.send({ success: false, msg: '잘못된 요청입니다' })
+  if (!search) search = ''
+  order = parseInt(order)
+  limit = parseInt(limit)
+  skip = parseInt(skip)
+  const s = {}
+  s[sort] = order
 
   const f = {}
   if (_board) f._board = _board
+  let total = 0
 
-  Article.find(f).select('-content').populate('_user', '-pwd')
-    .then(rs => {
-      res.send({ success: true, ds: rs, token: req.token })
-    })
-    .catch(e => {
-      res.send({ success: false, msg: e.message })
-    })
-});
-router.get('/read/:_id', (req, res, next) => {
-  const _id = req.params._id
-
-  Article.findById(_id).select('content')
+  Article.countDocuments(f)//데이터 개수 구하기
+    .where('title').regex(search) // title에서 search로 넘어온 값이 포함된 녀석들만 구하기
     .then(r => {
-      res.send({ success: true, d: r, token: req.token })
+      total = r //조건을 통과한 모든 데이터 
+      return Article.find(f)
+        .where('title').regex(search)
+        .sort(s) //order순으로 정렬
+        .skip(skip)//??시작 부분을 설정함 
+        .limit(limit)//출력할 갯수 
+        //limit와 skip을 통해 보내줄 갯수를 제한할 수 있음 
+        .select('-content') //content를 제외
+        .populate('_user', '-pwd')
+    })
+    .then(rs => {
+      res.send({ success: true, t: total, ds: rs, token: req.token })
     })
     .catch(e => {
       res.send({ success: false, msg: e.message })
     })
 })
+
 router.put('/:_id', (req, res, next) => {
   if (!req.user._id) return res.send({ success: false, msg: '게시물 수정 권한이 없습니다' })
   const _id = req.params._id
