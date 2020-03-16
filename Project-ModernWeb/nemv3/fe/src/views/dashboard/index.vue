a32eace77e7ac6cec59370e3b993a19361bb35a7
kboysm <lishal12@naver.com>
2020-03-16 16:19:26 +0900

    dashboard 구역 나누기

--- Project-ModernWeb/nemv3/fe/src/views/dashboard/index.vue
+++ Project-ModernWeb/nemv3/fe/src/views/dashboard/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <v-container grid-list-md>
+  <v-container fluid :grid-list-md="!$vuetify.breakpoint.xs">
     <v-layout wrap row>
-      <v-flex xs12 sm6>
-        <link-card></link-card>
+      <v-flex xs12 sm6 md3 class="pb-2" v-for="i in 4" :key="i">
+        <v-card height="130px" :color="`#${i}00000`">
+          각종 현황
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm4 class="pb-2" v-for="i in 3" :key="i">
+        <v-card height="140px" :color="`#${i}12345`">
+          간단한 차트
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 class="pb-2" v-for="i in 2" :key="i">
+        <v-card height="290px" :color="`#${i}54321`">
+          링크, 최근 게시물 같은..
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
