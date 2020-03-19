95f3ad0e668aa0bcd194459dcc6b87867fbab6b5
kboysm <lishal12@naver.com>
2020-03-19 21:15:58 +0900

    git checkout 실수

--- nuxt/test/src/components/HelloWorld.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    firebase login<br>
-    firebase init<br>
-    firebase deploy <br>
-    dist 폴더를 firebase 서버에 올림<br>
-    dist폴더는 front를 build해서 만들면 댐
-  </div>
-</template>
--- nuxt/test/src/views/e404.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>그런 페이지 없구요~</h1>
-  </div>
-</template>
