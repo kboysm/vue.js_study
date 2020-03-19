8abcdfbc83a101f011b5cf9fe9d22c32957659ab
kboysm <lishal12@naver.com>
2020-03-19 21:16:58 +0900

    git checkout 실수 복구

---
+++ nuxt/test/src/App.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-toolbar flat class="transparent">
+        <v-toolbar-title>Account</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-group
+          v-for="item in items"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="item.icon"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+
+          <v-list-tile
+            v-for="subItem in item.subItems"
+            :key="subItem.title"
+            :to="subItem.to"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>{{ subItem.icon }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+        <!-- <v-list-tile v-for="item in items" :key="item.title" :to="item.to">
+          <v-list-tile-avatar>
+            <v-icon>mdi-account</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile> -->
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="lime">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon :to="{ name: 'About' }">
+        <v-icon>mdi-account-search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+export default {
+  name: "App",
+  // components: {
+  //   HelloWorld
+  // },
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: "mdi-alert",
+          title: "about",
+          active: true,
+          subItems: [
+            {
+              title: "dashboard",
+              to: "/"
+            },
+            {
+              title: "About",
+              to: "/about"
+            }
+          ]
+        },
+        {
+          icon: "mdi-alert",
+          title: "Lectures",
+          active: false,
+          subItems: [
+            {
+              title: "card",
+              to: "/lectures/card"
+            },
+            {
+              title: "layout",
+              to: "/lectures/layout"
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    test() {
+      this.drawer = !this.drawer;
+    }
+  }
+};
+</script>
---
+++ nuxt/test/src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-tile v-for="item in items" :key="item.title">
+        <v-list-tile-avatar>
+          <v-icon>mdi-account</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>{{item.title}}</v-list-tile-title>
+          <!-- <v-list-tile-sub-title>subTitle</v-list-tile-sub-title> -->
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: "mdi-alert",
+          title: "about"
+        },
+        {
+          icon: "mdi-alert",
+          title: "LSM"
+        },
+        {
+          icon: "mdi-alert",
+          title: "GOOD"
+        }
+      ]
+    };
+  }
+};
+</script>
