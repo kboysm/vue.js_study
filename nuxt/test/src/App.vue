8ea0bbd0b52011702e6d5734f5dd94aee7360141
kboysm <lishal12@naver.com>
2020-03-19 16:13:54 +0900

    firebase & vue start

--- nuxt/test/src/App.vue
+++ nuxt/test/src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>firebase</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-toolbar dark color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
 
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+// import HelloWorld from './components/HelloWorld'
+import 'vue-material/dist/vue-material.min.css'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
+  // components: {
+  //   HelloWorld
+  // },
   data () {
     return {
       //
---
+++ nuxt/test/src/views/e404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>그런 페이지 없구요~</h1>
+  </div>
+</template>
