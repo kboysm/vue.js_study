8b7fc73af783e9ac38acfc37cc46ad6b0e67a14c
kboysm <lishal12@naver.com>
2020-03-23 20:29:54 +0900

    axios interceptor

--- nuxt/test/src/plugins/axios.js
+++ nuxt/test/src/plugins/axios.js
@@ -7,4 +7,14 @@ const firebaseAPI = axios.create({
     headers: { "Custom-Header": "LSM" },
 })
 
+firebaseAPI.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    config.headers.authorization = Math.random()
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+
 Vue.prototype.$axios = firebaseAPI
