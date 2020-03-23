ba829605e66353c9bb7abd7f20a45dcf7ae6dc2f
kboysm <lishal12@naver.com>
2020-03-23 20:08:34 +0900

    axios conf

--- nuxt/test/src/plugins/axios.js
+++ nuxt/test/src/plugins/axios.js
@@ -1,3 +1,10 @@
 import axios from 'axios'
 import Vue from 'vue'
-Vue.prototype.$axios = axios
+
+const firebaseAPI = axios.create({
+    baseURL: "http://localhost:5001/test-vf-lsm-ae9ca/us-central1/",
+    timeout: 5000,
+    headers: { "Custom-Header": "LSM" },
+})
+
+Vue.prototype.$axios = firebaseAPI
