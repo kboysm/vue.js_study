815813febc4af5246d5ac5811fe4e10d88f4e942
kboysm <lishal12@naver.com>
2020-11-23 21:14:00 +0900

    사용자 디렉티브

--- official-vue/src/main.ts
+++ official-vue/src/main.ts
@@ -4,7 +4,18 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+// 사용자 지정 디렉티브 테스트
+Vue.directive('test-directive', (el, binding) => {
+  console.log("test!el : ", el);
+  console.log("test!binding : ", binding);
+  el.style.backgroundColor = binding.value.color
+  el.style.color = binding.value.textColor
+}) // v-사용자지정 디렉티브로 인자를 넘겨서 활용가능
 
+// Vue.directive('test-directive', (el) => {
+//   el.focus();
+// })
+// 포커스가 걸리는걸 확인했음 , 전역적 사용 가능
 new Vue({
   router,
   store,
--- official-vue/src/views/About.vue
+++ official-vue/src/views/About.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="about">
-    <essential-elements ref="testRef" style="display: none" v-bind:testData="testData" :numberTest.sync="numberTest" :objectTest="objectTest" limsm="true"></essential-elements>
+    <form>
+      <input type="text" v-test-directive="{ color: 'black', textColor:'white'}" />
+    </form>
+    <essential-elements
+      ref="testRef"
+      style="display: none"
+      v-bind:testData="testData"
+      :numberTest.sync="numberTest"
+      :objectTest="objectTest"
+      limsm="true"
+    ></essential-elements>
     <slot-test style="display: none;">
-      <template v-slot="{testSlotData : vSlotStudyTest}">
-        {{ vSlotStudyTest }}
-      </template>
-      <template v-slot="{testSlotDefaultData={testData:'1234'}}">
-        {{ testSlotDefaultData.testData }}
-      </template>
+      <template v-slot="{testSlotData : vSlotStudyTest}">{{ vSlotStudyTest }}</template>
+      <template v-slot="{testSlotDefaultData={testData:'1234'}}">{{ testSlotDefaultData.testData }}</template>
     </slot-test>
     <props-test style="display: none;" :test-props="testProps"></props-test>
-<!--
+    <!--
 <keep-alive>
   <component v-bind:is="currentTabComponent"></component>
 </keep-alive>
 상태를 유지할 수 있고 다시 렌더링하지도 않음.
--->
+    -->
     <css-transition-test></css-transition-test>
   </div>
 </template>
@@ -23,25 +29,28 @@
 import EssentialElements from "@/components/EssentialElements"
 import SlotTest from "@/components/SlotTest"
 import PropsTest from "@/components/PropsTest"
-import CssTransitionTest from "@/components/CssTransitionTest";
+import CssTransitionTest from "@/components/CssTransitionTest"
 export default {
   components: {
-    EssentialElements,SlotTest,PropsTest,CssTransitionTest
+    EssentialElements,
+    SlotTest,
+    PropsTest,
+    CssTransitionTest,
   },
   data() {
     return {
       testData: "testtest",
-      numberTest:1,
-      objectTest:{
-        test_0:'string test',
-        test_1:12,
-        test_2:''
+      numberTest: 1,
+      objectTest: {
+        test_0: "string test",
+        test_1: 12,
+        test_2: "",
       },
-      testProps:'-test'
+      testProps: "-test",
     }
   },
   mounted() {
     // console.log(this.$refs.testRef)
-  }
+  },
 }
 </script>
