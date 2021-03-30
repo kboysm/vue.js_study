2a4b80b26aec300fb70a1816bfbfef6576fb178d
kboysm <lishal12@naver.com>
2021-03-30 09:48:22 +0900

    watch,watchEffect , computed , reactive

--- vue3/README.md
+++ vue3/README.md
@@ -2,3 +2,51 @@
 
 ## vue2와 vue3 차이
 루트요소가 한개만 있어야한다 - vue2 , 루트 요소가 여러개 존재 가능하다 - vue3
+
+## 반응형 데이터
+```
+let name = 'limsm'
+let age = 55
+
+const handleClick = () => {
+    name = 'abcd'
+    age = 22
+}
+    --변하지 않음--
+
+const name = ref('limsm')
+const age = ref(35)
+
+const handleClick = () => {
+    name.value = 'abcd'
+    age.value = 22
+}
+변함 
+```
+반응형 - ref
+
+## ref , reactive
+
+ref : primitive type을 가변 참조 객체로 만들고 , 내부 값을 가리키는 단일 속성 value가 있다.
+
+reactive : 객체를 반응형으로 만든다. 객체를 사용하는 경우 reactive를 사용하면 되지만 , 반환된 객체에 대한 참조유지를 위해 객체를 해제하거나 펼치는 경우 toRefs()를 사용한다.
+
+## watch , watchEffect
+
+watchEffect : 여러 반응 속성을 보고 싶고 이전 값을 신경 쓰지 않을 때
+watch : 특정 반응 속성을 보고 싶을 때 이전 값이 필요할 때
+
+watch 중지하는 법
+
+```
+const stopWatchEffect = watchEffect(() => {
+      console.log('watchEffect function ran' , search.value)
+    })
+    const matchingNames = computed(()=> {
+      return names.value.filter( name => name.includes( search.value ))
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopWatchEffect()
+    }
+```
--- vue3/src/views/Home.vue
+++ vue3/src/views/Home.vue
@@ -1,29 +1,58 @@
 <template>
   <div class="home">
-    home
+    <!-- home
     <p ref="p"> name : {{ name }} And age: {{age}}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">age plus</button>
+    <input type="text" v-model="name"> -->
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <h1>Home</h1>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    // console.log('setup')
 
-    const p = ref("")
-    let name = 'lim'
-    let age = 29
+    // const p = ref("")
+    // const name = ref('lim')
+    // const age = ref(29)
 
-    const handleClick= () => {
-      console.log(p.value)
-      p.value.classList.add('test')
+    // const handleClick= () => {
+    //   // console.log(p.value)
+    //   // p.value.classList.add('test')
+    //   name.value = 'limsm'
+    //   age.value = 55
+    // }
+    const search = ref('')
+    const names = ref(['aaa','bbb','ccc','ddd','eee','fff','ggg'])
+    const stopWatch = watch(search , () => {
+      console.log('watch function ran')
+    })
+
+    const stopWatchEffect = watchEffect(() => {
+      console.log('watchEffect function ran' , search.value)
+    })
+    const matchingNames = computed(()=> {
+      return names.value.filter( name => name.includes( search.value ))
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopWatchEffect()
+      
     }
-
     return {
-      name , age , handleClick , p // key:value
+      // name , age , handleClick , p // key:value
+      names,
+      search,
+      matchingNames,
+      handleClick
     }
   }
 }
